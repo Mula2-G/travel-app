50274e8b042f606591cb4dd5fcb619e1ac0ea02a
Mula2-G <@muakasahotmail.frgit config --global user.email muakasa@hotmail.frgit config --global user.email @muakasahotmail.fr>
2020-09-13 17:33:45 +0200

    started the testimonials styling

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,4 +1,7 @@
 import '../styles/style.css'
+import MobilMenu from './modules/mobileMenu'
+
+let mobileMenu = new MobilMenu();
 
 //accept the hot updates if it makes sense to accept them
 if (module.hot) {
